43b6b79d61e2259ed34aec76e025b590ac876b37
chongcher <ccchia.2014@smu.edu.sg>
2016-11-19 11:33:11 +0800

    Added Ranging functionality again

---
+++ app/src/main/java/com/example/hp/myapplication/Volunteer/DetectedMissingBeaconDialogFragment.java
@@ -0,0 +1,64 @@
+package com.example.hp.myapplication.Volunteer;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.example.hp.myapplication.R;
+
+/**
+ * Created by chongcher on 19/11/2016.
+ */
+
+public class DetectedMissingBeaconDialogFragment extends DialogFragment {
+
+    /* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface MissingBeaconDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    MissingBeaconDialogListener mListener;
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (MissingBeaconDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement MissingBeaconDialogListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Found a missing beacon!")
+                .setPositiveButton("Track", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        mListener.onDialogPositiveClick(DetectedMissingBeaconDialogFragment.this);
+                    }
+                })
+                .setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                        mListener.onDialogNegativeClick(DetectedMissingBeaconDialogFragment.this);
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+}
